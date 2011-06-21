ed97af954e14c5a6e3e5d7a77c70c994e0390e34
Oleg Zee <olegzzzz@gmail.com>
2011-06-21 19:20:35 +0700

    implemented GPU renderer

---
+++ src/FractalBrowser/Fractal/LyapRendererBase.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Drawing;
+
+namespace OlegZee.FractalBrowser.Fractal
+{
+	/// <summary>
+	/// CPU based renderer implementation
+	/// </summary>
+	public abstract class LyapRendererBase : FractalRenderer<Lyapunov.Settings>
+	{
+		protected const double Contrast = 1.7;
+
+		public override Bitmap Render(Lyapunov.Settings settings)
+		{
+			return Render(settings, d => ColorFromExp(d, settings.Contrast));
+		}
+
+		#region Implementation
+
+		private static Color ColorFromExp(double lyapExp, double contrast)
+		{
+			int colorIntensity;
+			if (Double.IsNegativeInfinity(lyapExp) || Double.IsPositiveInfinity(lyapExp))
+			{
+				return Color.Black;
+			}
+			if (Double.IsNaN(lyapExp))
+			{
+				return Color.White;
+			}
+			if (lyapExp > 0)
+			{
+				colorIntensity = (int)(Math.Exp(-lyapExp) * 255);
+				return Color.FromArgb(255, 0, 0, 255 - colorIntensity);
+			}
+
+			colorIntensity = (int)(Math.Exp(lyapExp) * 255);
+			colorIntensity = (int)(Math.Pow(colorIntensity, contrast) / Math.Pow(255, (contrast - 1)));
+
+			if (colorIntensity > 255)
+				colorIntensity = 255;
+			else if (colorIntensity < 0)
+				colorIntensity = 0;
+
+			return Color.FromArgb(255, colorIntensity, (int)(colorIntensity * .85), 0);
+		}
+
+		#endregion
+	}
+}
--- src/FractalBrowser/View/Form1.Designer.cs
+++ src/FractalBrowser/View/Form1.Designer.cs
@@ -34,6 +34,8 @@
 			this.comboBoxRenderer = new System.Windows.Forms.ComboBox();
 			this.label1 = new System.Windows.Forms.Label();
 			this.buttonBenchmark = new System.Windows.Forms.Button();
+			this.comboboxPicSize = new System.Windows.Forms.ComboBox();
+			this.label2 = new System.Windows.Forms.Label();
 			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
 			this.SuspendLayout();
 			// 
@@ -55,7 +57,7 @@
 			this.btnStart.Location = new System.Drawing.Point(25, 34);
 			this.btnStart.Name = "btnStart";
 			this.btnStart.Size = new System.Drawing.Size(75, 23);
-			this.btnStart.TabIndex = 2;
+			this.btnStart.TabIndex = 0;
 			this.btnStart.Text = "Start";
 			this.btnStart.UseVisualStyleBackColor = true;
 			this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
@@ -69,7 +71,7 @@
 			this.listBoxLog.Location = new System.Drawing.Point(12, 372);
 			this.listBoxLog.Name = "listBoxLog";
 			this.listBoxLog.Size = new System.Drawing.Size(403, 160);
-			this.listBoxLog.TabIndex = 3;
+			this.listBoxLog.TabIndex = 6;
 			// 
 			// comboBoxRenderer
 			// 
@@ -78,7 +80,7 @@
 			this.comboBoxRenderer.Location = new System.Drawing.Point(273, 36);
 			this.comboBoxRenderer.Name = "comboBoxRenderer";
 			this.comboBoxRenderer.Size = new System.Drawing.Size(142, 21);
-			this.comboBoxRenderer.TabIndex = 4;
+			this.comboBoxRenderer.TabIndex = 3;
 			// 
 			// label1
 			// 
@@ -86,24 +88,44 @@
 			this.label1.Location = new System.Drawing.Point(213, 39);
 			this.label1.Name = "label1";
 			this.label1.Size = new System.Drawing.Size(54, 13);
-			this.label1.TabIndex = 5;
+			this.label1.TabIndex = 2;
 			this.label1.Text = "Renderer:";
 			// 
 			// buttonBenchmark
 			// 
-			this.buttonBenchmark.Location = new System.Drawing.Point(25, 63);
+			this.buttonBenchmark.Location = new System.Drawing.Point(25, 318);
 			this.buttonBenchmark.Name = "buttonBenchmark";
 			this.buttonBenchmark.Size = new System.Drawing.Size(75, 23);
-			this.buttonBenchmark.TabIndex = 2;
+			this.buttonBenchmark.TabIndex = 1;
 			this.buttonBenchmark.Text = "Benchmark";
 			this.buttonBenchmark.UseVisualStyleBackColor = true;
 			this.buttonBenchmark.Click += new System.EventHandler(this.buttonBenchmark_Click);
+			// 
+			// comboboxPicSize
+			// 
+			this.comboboxPicSize.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.comboboxPicSize.FormattingEnabled = true;
+			this.comboboxPicSize.Location = new System.Drawing.Point(273, 64);
+			this.comboboxPicSize.Name = "comboboxPicSize";
+			this.comboboxPicSize.Size = new System.Drawing.Size(104, 21);
+			this.comboboxPicSize.TabIndex = 5;
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(213, 67);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(30, 13);
+			this.label2.TabIndex = 4;
+			this.label2.Text = "Size:";
 			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(974, 564);
+			this.Controls.Add(this.comboboxPicSize);
+			this.Controls.Add(this.label2);
 			this.Controls.Add(this.label1);
 			this.Controls.Add(this.comboBoxRenderer);
 			this.Controls.Add(this.listBoxLog);
@@ -127,6 +149,8 @@
 		private System.Windows.Forms.ComboBox comboBoxRenderer;
 		private System.Windows.Forms.Label label1;
 		private System.Windows.Forms.Button buttonBenchmark;
+		private System.Windows.Forms.ComboBox comboboxPicSize;
+		private System.Windows.Forms.Label label2;
 	}
 }
 
