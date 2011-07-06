126d972957837e87666482c7e5db1a040e753cab
Oleg Zee <olegzzzz@gmail.com>
2011-07-07 00:50:32 +0700

    fixed performance degradation

--- src/FractalBrowser/Fractal/Lyapunov.c
+++ src/FractalBrowser/Fractal/Lyapunov.c
@@ -4,30 +4,28 @@
 	constant float* m,
 	global write_only float* t,
 	float initialX,
-	int warmupCount,
-	int iterationsCount,
-	int maskLen)
+	int warmupCount, int iterationsCount,
+	int maskLen, int columns, float divider)
 {
 	int i = get_global_id(0);
 	int j = get_global_id(1);
 	float x = initialX;
-
+	float r = 0.0f;
 	float bv = b[i];
 	float av = a[j];
 	for (int idx = 0; idx < warmupCount; idx++)
 	{
-		float r = m[idx % maskLen] == 0 ? av : bv;
+		r = m[idx % maskLen] == 0 ? av : bv;
 		x = r * x - r * x * x;
 	}
 
-	float total = 0;
+	float total = 0.0f;
 	for (int idx = warmupCount; idx < iterationsCount; idx++)
 	{
-		float r = m[idx % maskLen] == 0 ? av : bv;
-		total = total + log(fabs(r - r * x * 2));
+		r = m[idx % maskLen] == 0 ? av : bv;
+		total = total + native_log(fabs(r - r * x * 2.0f));
 		x = r * x - r * x * x;
 	}
 
-	int columns = get_global_size(0);
-	t[j * columns + i] = total / (iterationsCount - warmupCount);
+	t[j * columns + i] = total * divider;
 }
